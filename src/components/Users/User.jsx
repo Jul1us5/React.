720b74aebc269d892f8c9e1b97ba372a3708e440
Julius <Julius>
2020-10-16 19:42:20 +0300

    mdsp

--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import style from "./User.module.scss";
 
 let User = (props) => {
-  console.log(props);
+  
   let printUser = props.users.user.map((el, i) => (
+    
     <OneMember
       key={i}
       title={el.title}
@@ -11,6 +12,10 @@ let User = (props) => {
       lastname={el.lastname}
       img={el.img}
       preview={el.preview}
+      or={el.follow}
+      x={el.id}
+      follow={props.follow}
+      unfollow={props.unfollow}
     />
   ));
 
@@ -18,6 +23,7 @@ let User = (props) => {
 };
 
 const OneMember = (props) => {
+  console.log(props.or);
   return (
     <div className={style.user}>
       <div className={style.head}>
@@ -31,10 +37,14 @@ const OneMember = (props) => {
       </div>
       <div className={style.title}>{props.title}</div>
       <div className={style.follow}>
-        <button>Follow</button>
+        {props.or 
+        ? <button onClick={ () => {props.follow(props.x) } }>FOLLOW</button> 
+        : <button onClick={ () => {props.unfollow(props.x) } }>UNFOLLOW</button>}
+        
       </div>
     </div>
   );
 };
+debugger
 
 export default User;
