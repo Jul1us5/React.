1e54118de8db3cd04deb3765ed5471374329e1a4
Julius <Julius>
2020-10-19 01:21:26 +0300

    Refactoring

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import style from "./Settings.module.scss";
 
-const Settings = (props) => {
-  return <section className={style.settings}>Settings</section>;
-};
+class Settings extends React.Component {
+  constructor(props) {
+    super();
+  }
+  render() {
+    return <section className={style.settings}>Settings</section>;
+  }
+}
 
 export default Settings;
--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -21,23 +21,12 @@ class User extends React.Component {
           </div>
           <div className={style.title}>{u.title}</div>
           <div className={style.follow}>
-            {u.follow ? (
-              <button
-                onClick={() => {
-                  this.props.follow(u.id);
-                }}
-              >
-                FOLLOW
-              </button>
-            ) : (
-              <button
-                onClick={() => {
-                  this.props.unfollow(u.id);
-                }}
-              >
-                UNFOLLOW
-              </button>
-            )}
+            {u.follow
+               ? (
+              <button onClick={() => { this.props.follow(u.id) }}> FOLLOW </button> 
+              ) : (
+              <button onClick={() => { this.props.unfollow(u.id) }}> UNFOLLOW </button> 
+              )}
           </div>
         </div>
       );
@@ -46,41 +35,3 @@ class User extends React.Component {
 }
 
 export default User;
-
-// let User = (props) => {
-// return props.users.user.map((u) => {
-//   return (
-//     <div key={u.id} className={style.user}>
-//       <div className={style.head}>
-//         <img src={u.preview} alt="img" />
-//       </div>
-//       <div className={style.img}>
-//         <img src={u.img} alt="img" />
-//       </div>
-//       <div className={style.who}>
-//         {u.firstname} {u.lastname}
-//       </div>
-//       <div className={style.title}>{u.title}</div>
-//       <div className={style.follow}>
-//         {u.follow ? (
-//           <button
-//             onClick={() => {
-//               props.follow(u.id);
-//             }}
-//           >
-//             FOLLOW
-//           </button>
-//         ) : (
-//           <button
-//             onClick={() => {
-//               props.unfollow(u.id);
-//             }}
-//           >
-//             UNFOLLOW
-//           </button>
-//         )}
-//       </div>
-//     </div>
-//   );
-// });
-// };
